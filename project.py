90f015370fb470a547c1d00348d6cc5180c83493
Craig Vidler <craig@craigvidler.co.uk>
2023-10-25 23:20:46 +0100

    Now using ANSI codes to colour the output 'cue' letters in green, yellow and grey boxes. Starting to look more like the real thing.

--- project.py
+++ project.py
@@ -1,11 +1,17 @@
 from random import choice
 
-with open('data/guesses.txt') as g, open('data/answers.txt') as a:
-    guesses = set(g.read().splitlines())
-    answer = choice(a.read().splitlines())
+BG_GREEN = '\u001b[48;5;28m'
+BG_YELLOW = '\u001b[48;5;11m'
+BG_GREY = '\u001b[48;5;239m'
+BOLD = '\u001b[1m'
+RESET = '\u001b[0m'
 
 attempt = 1
 attempts = 6
+
+with open('data/guesses.txt') as g, open('data/answers.txt') as a:
+    guesses = set(g.read().splitlines())
+    answer = choice(a.read().splitlines())
 
 while attempt <= attempts:
 
@@ -15,11 +21,6 @@ while attempt <= attempts:
     if guess not in guesses:
         print('Not a valid word')
         continue
-
-    # If all chars located correctly (all green)
-    if guess == answer:
-        print('Correct!')
-        raise SystemExit()
 
     # `answer` cast to list as we want to delete found letters. Given answer
     # of eg 'coder', deletion avoids eg the second 'e' in 'erase' wrongly
@@ -31,16 +32,22 @@ while attempt <= attempts:
         # letter located correctly (green)
         if letter == answer_letters[index]:
             answer_letters[index] = ' '
-            print(f'{letter}: green')
+            print(f'{BG_GREEN}{BOLD} {letter.upper()} {RESET} ', end='')
 
         # letter present but not located correctly (yellow)
         elif letter in answer_letters:
             answer_letters[answer_letters.index(letter)] = ' '
-            print(f'{letter}: yellow')
+            print(f'{BG_YELLOW}{BOLD} {letter.upper()} {RESET} ', end='')
 
         # letter not present at all (grey)
         else:
-            print(f'{letter}: grey')
+            print(f'{BG_GREY}{BOLD} {letter.upper()} {RESET} ', end='')
+    print()
+
+    # If all chars located correctly (all green)
+    if guess == answer:
+        print('Correct!')
+        raise SystemExit()
 
     attempt += 1
 
