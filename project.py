16c05ecea96b93e57213d39caef24a0e36933bbc
Craig Vidler <craig@craigvidler.co.uk>
2023-10-27 00:32:48 +0100

    Program now refactored into functions, based around reading files, taking input, game logic, and output. Works, but ugly.

--- project.py
+++ project.py
@@ -1,55 +1,85 @@
 from random import choice
 
-BG_GREEN = '\u001b[48;5;28m'
-BG_YELLOW = '\u001b[48;5;11m'
-BG_GREY = '\u001b[48;5;239m'
-BOLD = '\u001b[1m'
-RESET = '\u001b[0m'
 
-attempt = 1
-attempts = 6
+def valid_guesses():
+    with open('data/guesses.txt') as f:
+        return set(f.read().splitlines())
 
-with open('data/guesses.txt') as g, open('data/answers.txt') as a:
-    guesses = set(g.read().splitlines())
-    answer = choice(a.read().splitlines())
 
-while attempt <= attempts:
+def choose_answer():
+    with open('data/answers.txt') as f:
+        return choice(f.read().splitlines())
 
-    # Get input
+
+MAX_ATTEMPTS = 6
+GUESSES = valid_guesses()
+ANSWER = choose_answer()
+
+
+def get_guess(attempt):
     guess = input(f'Attempt #{attempt}: ').lower()
+    return guess if guess in GUESSES else False
 
-    if guess not in guesses:
-        print('Not a valid word')
-        continue
 
-    # `answer` cast to list as we want to delete found letters. Given answer
-    # of eg 'coder', deletion avoids eg the second 'e' in 'erase' wrongly
-    # being marked yellow.
-    answer_letters = list(answer)
+def score(guess):
+    # set letters to grey by default, cast answer to list so we can delete
+    # letters as we go to avoid counting dupes
+    scored_guess = [(letter, 'GREY') for letter in guess]
+    answer_letters = list(ANSWER)
 
     for index, letter in enumerate(guess):
 
         # letter located correctly (green)
         if letter == answer_letters[index]:
-            answer_letters[index] = ' '
-            print(f'{BG_GREEN}{BOLD} {letter.upper()} {RESET} ', end='')
+            scored_guess[index] = (letter, 'GREEN')
+            answer_letters[index] = ''
 
         # letter present but not located correctly (yellow)
         elif letter in answer_letters:
-            answer_letters[answer_letters.index(letter)] = ' '
-            print(f'{BG_YELLOW}{BOLD} {letter.upper()} {RESET} ', end='')
+            scored_guess[index] = (letter, 'YELLOW')
+            answer_letters[answer_letters.index(letter)] = ''
 
-        # letter not present at all (grey)
-        else:
-            print(f'{BG_GREY}{BOLD} {letter.upper()} {RESET} ', end='')
+    return scored_guess
+
+
+def output(scored_guess):
+    colours = {
+        'GREEN': '\u001b[48;5;28m',
+        'YELLOW': '\u001b[48;5;11m',
+        'GREY': '\u001b[48;5;239m'
+    }
+    bold = '\u001b[1m'
+    reset = '\u001b[0m'
+
+    greens = 0
+    for letter, colour in scored_guess:
+        print(f'{colours[colour]}{bold} {letter.upper()} {reset}', end='')
+        if colour == 'GREEN':
+            greens += 1
     print()
 
     # If all chars located correctly (all green)
-    if guess == answer:
+    if greens == len(scored_guess):
         print('Correct!')
         raise SystemExit()
 
-    attempt += 1
 
-# Word not guessed, game over
-print(f"Game over. The word was '{answer}'.")
+def main():
+    attempt = 1
+    print(ANSWER)
+    while attempt <= MAX_ATTEMPTS:
+        guess = get_guess(attempt)
+        if not guess:
+            print('Not a valid word')
+            continue
+        scored_guess = score(guess)
+        output(scored_guess)
+
+        attempt += 1
+
+    # Word not guessed, game over
+    print(f"Game over. The word was '{ANSWER}'.")
+
+
+if __name__ == '__main__':
+    main()
