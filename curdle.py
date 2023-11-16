1665c074c40f46acca1742efe98e83800930061c
Craig Vidler <craig@craigvidler.co.uk>
2023-11-16 18:50:25 +0000

    Title bar, guesses table and letter tracker now in windows of their own, simplifying writing to them (a bit), hopefully forming a base for further structural improvements.

--- curdle.py
+++ curdle.py
@@ -8,9 +8,9 @@ from enum import IntEnum
 
 class Color:
     """
-    Set up curses color pairs in a global Color object with dot syntax.
-    Since colors can't be set up till after curses is loaded, using a global
-    enum as we'd like isn't simple. This is close and less faff.
+    Set up curses color pairs in a global Color object with dot syntax. Since
+    colors can't be set up till after curses is loaded, using a simple global
+    enum as we'd like isn't easy. This is close and less faff.
 
     Example: 'BL_WHITE': (1, 234, 255) ->
     curses.init_pair(1, 234, 255)
@@ -43,28 +43,32 @@ class Color:
 class Curdle:
     def __init__(self, stdscr):
         self.stdscr = stdscr
-        self.guess_x = None
+        self.height, self.width = stdscr.getmaxyx()
         self.timer = None
-        self.popup = curses.newwin(1, 21, 17, curses.COLS // 2 - 10)
 
-    def draw_title_bar(self):
-        addstr = self.stdscr.addstr
+        # windows
+        self.title = curses.newwin(1, self.width + 1, 0, 0)  # needs + 1 to fill width?!
+        self.guesses = curses.newwin(12, 19, 5, self.width // 2 - 9)
+        self.popup = curses.newwin(1, 21, 17, self.width // 2 - 10)
+        self.letter_tracker = curses.newwin(5, 39, 19, self.width // 2 - 19)
+
+    def draw_title(self):
         title = 'curdle'
-        addstr(0, 0, ' ' * curses.COLS, Color.WH_DGREY)
-        addstr(0, curses.COLS // 2 - len(title) // 2, title, Color.WH_DGREY)
+        self.title.addstr(0, 0, ' ' * (self.width), Color.WH_DGREY)
+        self.title.addstr(0, self.width // 2 - len(title) // 2, title, Color.WH_DGREY)
 
         menu = '<esc> for menu'
-        addstr(0, curses.COLS - len(menu) - 1, '<esc>', Color.WH_DGREY)
-        addstr(' for menu', Color.LG_DGREY)
+        self.title.addstr(0, self.width - len(menu) - 1, '<esc>', Color.WH_DGREY)
+        self.title.addstr(' for menu', Color.LG_DGREY)
+        self.title.refresh()
 
-    def draw_guesses_board(self):
-        guess_width = 19
-        self.guess_x = curses.COLS // 2 - guess_width // 2
+    def draw_guesses(self):
 
         for i in range(6):
-            y = 5 + i * 2
+            y = i * 2
             for j in range(5):
-                self.stdscr.addstr(y, self.guess_x + j * 4, '   ', Color.BL_WHITE)
+                self.guesses.addstr(y, j * 4, '   ', Color.BL_WHITE)
+        self.guesses.refresh()
 
     def show_popup(self, message, duration=2.5):
 
@@ -86,21 +90,20 @@ class Curdle:
 
     def draw_tracker(self, tracker=None):
 
-        center_x = curses.COLS // 2
-        tracker_width = 39
-        tracker_x = center_x - tracker_width // 2
-
         letters = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+        x = 0
 
         for i, row in enumerate(letters):
-            y = 19 + i * 2
+            y = i * 2
             if i == 1:
-                tracker_x += 2
+                x += 2
             if i == 2:
-                tracker_x += 4
+                x += 4
             for j, letter in enumerate(row):
                 color = Color.BL_LGREY if not tracker else Color.letter_colors[tracker[letter]]
-                self.stdscr.addstr(y, tracker_x + j * 4, f' {letter.upper()} ', color)
+                self.letter_tracker.addstr(y, x + j * 4, f' {letter.upper()} ', color)
+
+        self.letter_tracker.refresh()
 
     def do_round(self, guess):
 
@@ -113,7 +116,7 @@ class Curdle:
             # Get input key. try/except here because window resize will crash
             # getkey() without it.
             try:
-                key = self.stdscr.getkey()
+                key = self.guesses.getkey()
             except curses.error:
                 pass
 
@@ -121,12 +124,12 @@ class Curdle:
             if key in ascii_letters and length < 5:
                 guess += key.lower()
                 letter = f' {key.upper()} '
-                self.stdscr.addstr(5 + (wordle.round - 1) * 2, self.guess_x + length * 4, letter, Color.BL_WHITE)
+                self.guesses.addstr((wordle.round - 1) * 2, length * 4, letter, Color.BL_WHITE)
 
             # if BACKSPACE (KEY_BACKSPACE Win/Lin; `\x7F` Mac; '\b' just in case)
             elif key in ('KEY_BACKSPACE', '\x7F', '\b') and guess:
                 guess = guess[:-1]
-                self.stdscr.addstr(5 + (wordle.round - 1) * 2, self.guess_x + (length - 1) * 4, '   ', Color.BL_WHITE)
+                self.guesses.addstr((wordle.round - 1) * 2, (length - 1) * 4, '   ', Color.BL_WHITE)
 
             # if ENTER (should work cross-platform)
             elif key in ('\n', '\r'):
@@ -136,7 +139,7 @@ class Curdle:
     def menu(self):
         self.show_popup('[N]ew game | [Q]uit', duration=0)
         while True:
-            key = self.stdscr.getkey()
+            key = self.guesses.getkey()
             if key == 'q':
                 raise SystemExit()
             elif key == 'n':
@@ -146,15 +149,14 @@ class Curdle:
     def reset(self):
         wordle.new_game()
         curses.flushinp()  # prevent input buffer dumping into new game
-        self.draw_title_bar()
-        self.draw_guesses_board()
+        self.draw_title()
+        self.draw_guesses()
         self.clear_popup()
         self.draw_tracker()
 
     def run(self):
         curses.use_default_colors()  # is this necessary?
         curses.curs_set(False)  # no cursor
-
         Color.setup()
         self.reset()
 
@@ -166,12 +168,12 @@ class Curdle:
                 guess = ''
                 for i, (letter, score) in enumerate(scored_guess):
                     letter = f' {letter.upper()} '
-                    self.stdscr.addstr(5 + (wordle.round - 2) * 2, self.guess_x + i * 4, letter, Color.letter_colors[score])
+                    self.guesses.addstr((wordle.round - 2) * 2, i * 4, letter, Color.letter_colors[score])
             else:
                 self.show_popup(response)
 
             self.draw_tracker(wordle.letter_tracker)
-            self.stdscr.refresh()
+            self.guesses.refresh()
 
             # output message if solved or game over, enable menu
             if wordle.state != 'playing':
