95b550e14faf87ed7392c11695d166bfd5c079b3
Craig Vidler <craig@craigvidler.co.uk>
2023-11-17 10:59:47 +0000

    controller.py: type hinting and docstrings added.

--- controller.py
+++ controller.py
@@ -1,21 +1,34 @@
+from model import Wordle  # Just for typehint below. Good practice?
+
+
 class Controller:
-    def __init__(self, view, wordle):
+    def __init__(self, view, wordle: Wordle):
+        """Instantiate controller, set up view and model."""
+
         self.wordle = wordle
         self.view = view
 
     def menu(self):
-        self.view.popup('[N]ew game | [Q]uit', duration=0)
+        """Display menu and handle choices."""
+
+        self.view.popup('[N]ew game | [Q]uit', duration=0)  # FIXME: avoid hardcoding
         option = self.view.menu()
-        if option == 'q':
+
+        if option == 'q':  # FIXME: enum, and values not keys
             raise SystemExit()
         elif option == 'n':
             self.reset()
 
     def reset(self):
+        """
+        Reset game (model/view) for initial setup + to enable multiple games.
+        """
         self.wordle.new_game()
         self.view.reset()
 
     def run(self):
+        """Run the application main loop."""
+
         self.reset()
 
         # MAIN LOOP
@@ -31,7 +44,7 @@ class Controller:
                 self.view.guess = ''  # reset guess buffer for next turn
                 self.view.draw_scored_guess(scored_guess, turn)
             else:
-                self.view.popup(response)  # 'not in word list' error
+                self.view.popup(response)  # ie 'not in word list' error
 
             self.view.draw_tracker(self.wordle.tracker)
 
